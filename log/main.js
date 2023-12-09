7fb0e7ede500fad33b31d51dd1c80b51023c5e8b
nyin01 <79818377+nyin01@users.noreply.github.com>
2023-12-09 17:02:28 -0500

    clean up merge conflict

--- log/main.js
+++ log/main.js
@@ -5,6 +5,8 @@ import * as THREE from "three";
 import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { generate } from "./l_util";
+import { generateRain, generateSnow, generateWind } from './weather.js';
+
 
 let camera;
 let renderer;
@@ -66,101 +68,10 @@ function setupRenderer() {
 }
 
 function setupSkyBox() {
-  // Create materials for the skybox
-  const skyColor = new THREE.Color("lightblue");
-  const groundColor = new THREE.Color("darkgreen");
-  const skyboxMaterials = [
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
-
-// weather utils
-import { generateRain, generateSnow, generateWind } from './weather.js';
-
-// Scene
-const scene = new THREE.Scene();
-
-/**
- * PERSPECTIVE CAMERA
- * 
- *  1) Field of View is the extent of the scene that is seen on the display at any given moment. The value is in degrees.
- *  2) Aspect Ratio: use the width of the element divided by the height, or you'll get squished image
- *  3) Near: if objects are closer than 'near', won't be rendered
- *  4) Far: if objects are farther than 'far', won't be rendered
- */
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 20, 25);
-
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth * 0.9, window.innerHeight * 0.9 );
-document.body.appendChild(renderer.domElement);
-
-// Mesh, object only
-const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: 0x489e94,
-});
-const cube1 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube1.position.set(-5, 7, 12); // or set individual x, y, z like cube.position.x = 5;
-scene.add(cube1);
-
-// Mesh, wireframe only
-const cubeWireframeMaterial = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: true,
-});
-const cubeWireframe1 = new THREE.Mesh(cubeGeometry, cubeWireframeMaterial);
-cubeWireframe1.position.set(-5, 10, 12); 
-scene.add(cubeWireframe1);
-// Add lighting
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(1, 1, 1);
-scene.add(light);
-
-// Function to create a tree
-function createTree() {
-  // Create a cylinder to represent the trunk
-  const trunkGeometry = new THREE.CylinderGeometry(0.5, 0.5, 3, 32);
-  const trunkMaterial = new THREE.MeshLambertMaterial({ color: 0x8B4513 });
-  const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
-  trunk.position.set(0, 0, 0);
-  scene.add(trunk);
-
-  // Create a cone to represent the foliage
-  const foliageGeometry = new THREE.ConeGeometry(2, 4, 32);
-  const foliageMaterial = new THREE.MeshLambertMaterial({ color: 0x00FF00 });
-  const foliage = new THREE.Mesh(foliageGeometry, foliageMaterial);
-  foliage.position.set(0, 3, 0);
-  scene.add(foliage);
-}
-
-createTree();
-
-let foliagePosY = 3;
-
-// Grow tree when clicked
-function setupCounter(container) {
-  const foliageGeometry = new THREE.ConeGeometry(2, 4, 32);
-  const foliageMaterial = new THREE.MeshLambertMaterial({ color: 0x00FF00 });
-  // Append the counter button to the provided container
-  const counterButton = document.createElement('button');
-  counterButton.textContent = 'Grow';
-  container.appendChild(counterButton);
-  // Handle button click to grow the tree
-  counterButton.addEventListener('click', () => {
-    foliagePosY += 2;
-    const moreFoliage = new THREE.Mesh(foliageGeometry, foliageMaterial);
-    moreFoliage.position.set(0, foliagePosY, 0);
-    scene.add(moreFoliage);
-  });
-}
-
-
-// Create materials for the skybox
-const skyColor = new THREE.Color('lightblue');
-const groundColor = new THREE.Color('darkgreen');
-const skyboxMaterials = [
+    // Create materials for the skybox
+    const skyColor = new THREE.Color('lightblue');
+    const groundColor = new THREE.Color('darkgreen');
+    const skyboxMaterials = [
     new THREE.MeshBasicMaterial({ color: skyColor, side: THREE.BackSide }), // Left side
     new THREE.MeshBasicMaterial({ color: skyColor, side: THREE.BackSide }), // Right side
     new THREE.MeshBasicMaterial({ color: skyColor, side: THREE.BackSide }), // Top side
@@ -180,20 +91,6 @@ function setupControl() {
   controls.target.set(0, 0, 0); // Set the point at which the camera looks
   controls.update();
 }
-
-// function setupGeometry() {
-//   const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-//   const cubeMaterial = new THREE.MeshBasicMaterial({
-//     color: 0x489e94,
-//   });
-//   cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-//   cube2.position.set(-5, 13, 12);
-//   scene.add(cube2);
-//   // another one, added as separate meshes
-//   cube3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-//   cube3.position.set(30, 13, 12);
-//   scene.add(cube3);
-// }
 
 function buildTree(iteration, radius, decay, length) {
   // iteration = iteration < 2 ? 2 : iteration;
